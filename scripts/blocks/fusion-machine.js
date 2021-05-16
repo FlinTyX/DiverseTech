88c24370aa0c8fab67c98fad9bf1237fea7cfc7e
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-05-16 05:26:41 -0300

    Update fusion-machine.js

--- scripts/blocks/fusion-machine.js
+++ scripts/blocks/fusion-machine.js
@@ -6,6 +6,18 @@ const craftFx = new Effect(30, e => {
   let w = e.fout() * 10;
   Drawf.tri(i == 0 ? e.x + 7 : e.x - 7, e.y, w, h, i == 0 ? 0 : 180);
   Drawf.tri(e.x, i == 0 ? e.y - 7 : e.y + 7, w, h, i == 0 ? -90 : 90);
+    
+  Draw.color(Color.valueOf("6e7080"), Color.valueOf("f3f3f3"), Color.valueOf("ffffff"), e.fin());
+
+  e.scaled(7, i => {
+    Lines.stroke(3 * i.fout());
+    Lines.circle(e.x, e.y, 4 + i.fin() * 30);
+  });
+
+  Draw.color(Color.valueOf("f3f3f3"));
+  Angles.randLenVectors(e.id, 8, 2 + 30 * e.finpow(), (x, y) => {
+    Fill.circle(e.x+x, e.y+y, e.fout() * 2 + 0.5);
+  });
   }
 });
 
