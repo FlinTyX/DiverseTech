9c027364aed6655a5bc0f3a6c96593d44907cb13
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-05-16 05:29:47 -0300

    Create quantum-former.js

---
+++ scripts/blocks/quantum-former.js
@@ -0,0 +1,13 @@
+const craftFx = new Effect(40, e => {
+  Draw.color(Color.valueOf("6e7080"), Color.valueOf("f3f3f3"), Color.valueOf("ffffff"), e.fin());
+
+  Draw.color(Color.valueOf("f3f3f3"));
+  Angles.randLenVectors(e.id, 8, 2 + 30 * e.finpow(), (x, y) => {
+    Fill.circle(e.x+x, e.y+y, e.fout() * 2 + 0.5);
+  });
+});
+
+const quantumFo = extend(GenericCrafter, "quantum-former", {});
+quantumFo.craftEffect = craftFx;
+
+quantumFo.buildType = () => etend(GenericCrafter.GenericCrafterBuild, quantumFo, {});
