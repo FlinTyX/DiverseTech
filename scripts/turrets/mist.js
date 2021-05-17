9069e7c8ff40060f1856dac480f8661d34e45d0c
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-05-16 22:41:01 -0300

    Update mist.js

--- scripts/turrets/mist.js
+++ scripts/turrets/mist.js
@@ -128,7 +128,7 @@ const mist = extend(PowerTurret, "mist", {
   shootCone: 2,
   shootSound: Sounds.railgun,
   reloadTime: 210,
-  rotateSpeed: 2,
+  rotateSpeed: 4,
   shootShake: 6,
   recoilAmount: 5,
   coolantMultiplier: 1.1,
