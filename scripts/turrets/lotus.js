d2a07f6b7eacccafb408c2ecd4c36d9a2512df40
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-05-19 00:33:55 -0300

    Update lotus.js

--- scripts/turrets/lotus.js
+++ scripts/turrets/lotus.js
@@ -4,26 +4,28 @@ const fragB = extend(ContinuousLaserBulletType, {});
 fragB.length = 150;
 fragB.drawSize = 150;
 fragB.damage = 50;
+fraqB.lifetime = 100;
 let despawned = false;
 let rot = 0;
 
 const lBullet = extend(ArtilleryBulletType, {
-  despawned(b){
-    despawned = true;
-  },
   update(b){
     rot = rot + 0.5;
     if(despawned){
       for(let i = 0; i < 4; ++i){
         let ang = i * 90 + b.rotation();
+        despawned = false;
         fragB.create(b.owner, b.team, b.x, b.y, ang + rot);
       }
     }
+  },
+    despawned(b){
+    despawned = true;
   }
 });
 lBullet.damage = 10;
 lBullet.speed = 2.2;
-lBullet.lifetime = 80;
+lBullet.lifetime = 70;
 lBullet.width = 14;
 lBullet.height = 12;
 lBullet.collidesTiles = false;
@@ -35,7 +37,7 @@ const lotus = extend(PowerTurret, "lotus", {
   targetAir: false,
   shots: 1,
   inaccuracy: 3,
-  reloadTime: 120,
+  reloadTime: 300,
   shootShake: 7,
   range: 290,
   minRange: 65,
