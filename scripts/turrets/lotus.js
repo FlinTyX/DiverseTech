64fdd4c4a342697c4ccfc44b542a8595d6b57a46
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-05-21 14:51:36 -0300

    Update lotus.js

--- scripts/turrets/lotus.js
+++ scripts/turrets/lotus.js
@@ -21,9 +21,9 @@ const lotus = extend(LaserTurret, "lotus", {
 lotus.buildType = () => extend(LaserTurret.LaserTurretBuild, lotus, {
   updateTile(){
     this.super$updateTile();
-    if(this.isShooting() && this.power.status > 0 || this.wasShooting() && this.isControlled() && this.power.status > 0){
+    if(this.isShooting() && this.power.status > 0 || this.wasShooting && this.isControlled() && this.power.status > 0){
       this.rotation = this.rotation + 15;
-    } else if(!this.isShooting() || !this.wasShooting()){
+    } else if(!this.isShooting() || !this.wasShooting){
       this.rotation = this.rotation;
     }
   }
