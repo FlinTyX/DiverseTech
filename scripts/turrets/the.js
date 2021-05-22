c578e3dae4c1769d136b48c977463d206abd9869
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-05-22 00:34:26 -0300

    Update the.js

--- scripts/turrets/the.js
+++ scripts/turrets/the.js
@@ -1,49 +1,68 @@
-const b1 = extend(MissileBulletType, {
-  damage: 340,
-  speed: 1.2,
-  lifetime: 200,
-  homingPower: 0.1,
-  homingRange: 60,
-  width: 8,
-  height: 8,
-  
-  update(b){
-    this.super$update(b);
-    b.vel.lerpDelta(b, 2);
-  },
-  despawned(b){
-    b1.speed = 1.2;
-  },
-  hit(b){
-    b1.speed = 1.2;
-  }
+const fragB1 = extend(BasicBulletType, {
+  damage: 165,
+  speed: 4.5,
+  lifetime: 70,
+  height: 23,
+  width: 19,
+  homingPower: 0.2,
+  homingRange: 250,
+  trailEffect: Fx.artilleryTrail,
+  trailChance: 10,
+  weaveScale: 3,
+  weaveMag: 3,
+  lightning: 3.5,
+  lightningLenght: 10,
+  shrinkY: 0
 });
 
-const b2 = extend(MissileBulletType, {
-  damage: 110,
-  speed: 2.7,
-  lifetime: 200,
-  homingPower: 0.1,
-  homingRange: 60,
-  width: 8,
-  height: 8
+const  bullet1  = extend(BasicBulletType, {
+  damage: 90,
+  speed: 0.3,
+  lifetime: 110,
+  drag: 0.05,
+  height: 18,
+  width: 18,
+  collidesGround: false,
+  collides: false,
+  collidesAir: false,
+  weaveScale: 3,
+  weaveMag: 3,
+  lightning: 3,
+  lightningLenght: 8,
+  trailChance: 1,
+  fragBullets: 1,
+  fragCone: 1,
+  shrinkY: 0,
+  fragBullet: fragB1
 });
 
 const turret = extend(ItemTurret, "the", {
   shots: 1,
-  range: 250,
+  range: 220,
   shootCone: 2,
-  reloadTime: 210,
+  reloadTime: 110,
   rotateSpeed: 5,
   shootShake: 5,
+  recoilAmount: 1,
+  shots: 4,
+  inaccuracy: 12.5,
+  shootCone: 360,
+  spacing: 90,
+  shootLenght: 5,
   
   init(){
     this.ammo(
-      Vars.content.getByName(ContentType.item, "diversetech-hyper-alloy"), b1,
-      Vars.content.getByName(ContentType.item, "diversetech-nitinol"), b2
+      Vars.content.getByName(ContentType.item, "diversetech-hyper-alloy"), b1
     );
     this.super$init();
   }
 });
 
-turret.buildType = () => extend(ItemTurret.ItemTurretBuild, turret, {});
+turret.buildType = () => extend(ItemTurret.ItemTurretBuild, turret, {
+  uptateTile(){
+    this.super$updateTile();
+    if(this.hasAmmo()){
+      this.rotation = 5 + this.rotation;
+    }
+  }
+});
