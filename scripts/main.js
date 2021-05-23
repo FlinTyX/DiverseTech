e68e84029f668de74742032032dbaa67638ff3c6
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-05-23 16:17:41 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
-require("blocks/fusion-machine");
-require("blocks/quantum-former");
-require("turrets/mist");
-require("turrets/magma");
+//require stuff
+let scripts = Seq.with(
+  //blocks
+  "blocks/fusion-machine", "blocks/quantum-former",
+  //turrets
+  "turrets/mist", "turrets/magma"
+);
+scripts.each(e => {
+  require(e);
+});
+print("DiverseTech is loaded, are you sure that u will play this?");
+
+//client load info
+Events.on(WorldLoadEvent, () => {
+  Vars.ui.showInfoText("[#ff4444]Confirm[]\n DiverseTech Mod", "If you are playing this mod, be aware it is under testing and that it still in development. \n \n If you want to make a suggestion/pull request goto FlinTyX/DiverseTech.");
+});
