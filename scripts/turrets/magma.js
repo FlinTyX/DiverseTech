ac47ed1a5d9909145a96f66e2bb07a0fc56d297e
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-05-23 03:12:08 -0300

    Update and rename the.js to magma.js

--- scripts/turrets/magma.js
+++ scripts/turrets/magma.js
@@ -1,24 +1,26 @@
 const fragB1 = extend(BasicBulletType, {
-  damage: 165,
+  damage: 115,
   speed: 4.5,
   lifetime: 85,
   height: 23,
   width: 19,
   homingPower: 0.2,
-  homingRange: 250,
+  homingRange: 200,
   trailEffect: Fx.artilleryTrail,
   trailChance: 10,
   weaveScale: 3,
   weaveMag: 3,
   lightning: 3.5,
   lightningLenght: 10,
+  sprite: diversetech-arrow,
+  backColor: ffc999,
   shrinkY: 0
 });
 
 const  bullet1  = extend(BasicBulletType, {
-  damage: 90,
+  damage: 70,
   speed: 7,
-  lifetime: 230,
+  lifetime: 200,
   drag: 0.04,
   height: 18,
   width: 18,
@@ -30,21 +32,23 @@ const  bullet1  = extend(BasicBulletType, {
   lightning: 3,
   lightningLenght: 8,
   trailChance: 0,
-  fragBullets: 2,
-  fragCone: 360,
+  fragBullets: 1,
+  fragCone: 90,
   shrinkY: 0,
+  sprite: diversetech-smallArrow,
+  backColor: ffc999,
   fragBullet: fragB1
 });
 
-const turret = extend(ItemTurret, "the", {
+const turret = extend(ItemTurret, "magma", {
   shots: 3,
   range: 300,
-  reloadTime: 100,
+  reloadTime: 110,
   shootShake: 5,
   recoilAmount: 1.5,
-  shootCone: 90,
+  shootCone: 45,
   spread: 30,
-  inaccuracy: 10,
+  inaccuracy: 15,
   shootType: bullet1,
   
   init(){
