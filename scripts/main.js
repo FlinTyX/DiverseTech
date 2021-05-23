d70b42f2ef61944089e137539385bbf94c263fab
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-05-23 16:32:12 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,6 @@ print("DiverseTech is loaded, are you sure that u will play this?");
 let shutUp = false;
 Events.on(ClientLoadEvent, () => {
   if(!shutUp){
-    Vars.ui.showInfoText("[#ff4444]Confirm[]", "If you are playing this mod, be aware it is under testing and it still in development. \n \n If you want to make a suggestion/pull request go to FlinTyX/DiverseTech.");
+    Vars.ui.showInfoText("[#ff4444]DiverseTech[]", "If you are playing this mod, be aware it is under testing and it still in development. \n \n If you want to make a suggestion/pull request go to FlinTyX/DiverseTech.");
     shutUp = true;
 });
