13c4fb9eed2ba6c97a0ee83d146af94bec79cab2
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-05-27 02:18:51 -0300

    Update overvoltage.js

--- scripts/turrets/overvoltage.js
+++ scripts/turrets/overvoltage.js
@@ -27,25 +27,24 @@ const orb = extend(BasicBulletType, {
   lifetime: 210,
   width: 22,
   height: 22,
-  shrinkY: 1,
-  shrinkX: 1,
+  shrinkY: 0.1,
+  shrinkX: 0.1,
   sprite: "diversetech-orb",
   backColor: Color.valueOf("f3f3f3"),
   lightning: 0.2,
   collidesAir: true,
   collidesGround: true,
   collides: true,
-  splashRadius: 5,
-  fragBullets: 1,
-  fragBullet: despawnB,
   
   update(b){
     this.super$update(b);
-    if(Mathf.chance(0.2)){
+    if(Mathf.chance(0.28)){
       lightB.create(b.owner, b.team, b.x, b.y, Mathf.range(360));
-      if(Mathf.chance(0.2)) Effect.shake(6, 10, b.x, b.y);
+      if(Mathf.chance(0.23)) Effect.shake(6, 10, b.x, b.y);
     }
-  }
+  },
+  despawned(b){
+    for(let i = 0; i < Math.random(4); i++) despawnB.create(b.owner, b.team, b.x, b.y, Mathf.range(360));
 });
 
 const overvoltage = extend(PowerTurret, "overvoltage", {
@@ -63,13 +62,13 @@ const overvoltage = extend(PowerTurret, "overvoltage", {
 
 overvoltage.buildType = () => extend(PowerTurret.PowerTurretBuild, overvoltage, {
   updateTile(){
+    //this is so painful please kill me
     this.super$updateTile();
-    if(Mathf.chance(0.06)){
+    if(Mathf.chance(0.06) && this.power.status >= 1 || this.isShooting()){
+      this.isShooting() ? let rp = Math.random(8) : let rp = 1;
+      if(Mathf.chance(0.25)) Effect.shake(8, 10, this.x, this.y);
       let rand = this.rotation - 180 + Mathf.range(50);
-      if(this.power.status >= 1){
-        lightB.create(this, this.team, this.x + Angles.trnsx(rand, 20), this.y + Angles.trnsy(rand, 20), rand);
-        if(Mathf.chance(0.15)) Effect.shake(8, 10, this.x, this.y);
-      }
+      for(let i = 0; i < rp; i++) lightB.create(this, this.team, this.x + Angles.trnsx(rand, 20), this.y + Angles.trnsy(rand, 20), rand);
     }
   }
 });
