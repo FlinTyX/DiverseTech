c1ff101074859707a5d10b65674037fee24d2497
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-06-02 11:43:36 -0300

    Update functions.js

--- scripts/libs/functions.js
+++ scripts/libs/functions.js
@@ -1,1 +1,19 @@
-//  
+function plusRot(rot, n){
+    if(rot > 360 && n >= 0){
+        rot = n;
+    } else {
+        rot = rot + n;
+    }
+    return rot;
+}
+
+function alphaPlus(alpha, n){
+    if(alpha > 1 + n && n >= 0){
+        alpha = 1;
+    } else if(alpha < 0 - n && n <= 0){
+        alpha = 0;
+    } else {
+        alpha = alpha + n;
+    }
+    return alpha;
+}
