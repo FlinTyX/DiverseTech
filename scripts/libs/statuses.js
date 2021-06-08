5673a9c4cc93fb082ccb20ee48957d595192d0bf
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-06-07 23:37:15 -0300

    Create statuses.js

---
+++ scripts/libs/statuses.js
@@ -0,0 +1,25 @@
+const iceFx = new Effect(35, e => {
+  Draw.color(Color.valueOf("f3f3f3"));
+  Lines.stroke(0.6);
+  Angles.randLenVectors(e.id, 10, 2 + 25 * e.finpow(), (x, y) => {
+    Lines.spikes(e.x+x, e.y+y, e.fout() * 0.5, 0.5, 8);
+  });
+});
+
+const ice = extend(StatusEffect, "ice", {
+  damage: 0.2,
+  speedMultiplier: 0.1,
+  color: Color.white,
+  effect: iceFx,
+});
+
+//exports
+let statuses = Seq.with(
+  ice
+);
+
+statuses.each(e => {
+  module.exports = {
+    e : e
+  }
+});
